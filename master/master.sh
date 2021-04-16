cfafd4fe39721c963cd78c55b22ed764941bc1f7
Color <alloy.apple@gmail.com>
2021-04-16 09:32:09 +0800

    添加问题描述

--- master/master.sh
+++ master/master.sh
@@ -4,6 +4,10 @@ echo "host replication all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
 set -e
 # export PGPASSWORD="$POSTGRESQL_PASSWORD"
 # psql登录参考：https://stackoverflow.com/questions/3582552/what-is-the-format-for-the-postgresql-connection-string-url
+# If you look into the current docker-entrypoint.sh version, you will see, that during the execution of scripts in /docker-entrypoint-initdb.d PostgreSQL listens only on the UNIX socket, and the startup log says:
+
+# LOG:  listening on Unix socket "/var/run/postgresql/.s.PGSQL.5432"
+
 psql  -U postgres <<-EOSQL
     CREATE ROLE user_name1 REPLICATION LOGIN PASSWORD 'my_password';
     CREATE database oil with owner user_name1;
